a8eb507430ddfcc23e35bc5252d472258ea05a87
Isaac Rumbold <isaac.rumbold@precisionplanting.com>
2023-05-18 14:58:03 -0500

    Just testing to see if this works or not

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+"use strict";
 var _a, _b, _c;
-import { User, Budget } from "./classes.js";
+Object.defineProperty(exports, "__esModule", { value: true });
+var classes_js_1 = require("./classes.js");
 //Event Listeners for buttons throughout the site
 (_a = document.getElementById("createaccount")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", createAccount);
 (_b = document.getElementById("submitlogin")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", login);
@@ -8,10 +10,10 @@ window.addEventListener("load", loaduserdata);
 //validates account creation and creates account and stores information
 function createAccount() {
     //assigns varibles to user input
-    const username = document.getElementById("create-uname").value.toLowerCase();
-    const password = document.getElementById("create-pass").value;
-    const verify = document.getElementById("verify-pass").value;
-    const church = document.getElementById("church-name").value;
+    var username = document.getElementById("create-uname").value.toLowerCase();
+    var password = document.getElementById("create-pass").value;
+    var verify = document.getElementById("verify-pass").value;
+    var church = document.getElementById("church-name").value;
     // checks if username already exists
     if (localStorage.getItem(username) !== null) {
         alert("Username already exists. Try a different Username.");
@@ -24,9 +26,9 @@ function createAccount() {
     }
     //if checks are met, creates account
     else {
-        const user = new User(username, password, church);
-        const budgetlist = new Array();
-        const listname = user.getusername + "budgets";
+        var user = new classes_js_1.User(username, password, church);
+        var budgetlist = new Array();
+        var listname = user.getusername + "budgets";
         localStorage.setItem(username, JSON.stringify(user));
         localStorage.setItem(listname, JSON.stringify(budgetlist));
         alert("Account created successfully!");
@@ -36,17 +38,17 @@ function createAccount() {
 //validates user log in information
 function login() {
     //assigns varibles from user input
-    const username = document.getElementById("uname").value.toLowerCase();
-    const password = document.getElementById("password").value;
+    var username = document.getElementById("uname").value.toLowerCase();
+    var password = document.getElementById("password").value;
     //retreives user data from local storage
     if (localStorage.getItem(username) !== null) {
-        const userinfo = JSON.parse(localStorage.getItem(username));
-        const pass = userinfo.password;
-        const uname = userinfo.username;
+        var userinfo = JSON.parse(localStorage.getItem(username));
+        var pass = userinfo.password;
+        var uname = userinfo.username;
         //Validates log in
         if (username == uname && password == pass) {
             sessionStorage.setItem("useris", userinfo.username);
-            alert(`You have successfully loged into ${uname}!`);
+            alert("You have successfully loged into ".concat(uname, "!"));
             window.open("home.html", "_self");
         }
         else {
@@ -59,20 +61,20 @@ function login() {
 }
 //loads user data if on home.html page
 function loaduserdata() {
-    const regex1 = /home/;
-    const url = window.location.href;
+    var regex1 = /home/;
+    var url = window.location.href;
     //uses regex to check if on home.html page
     if (regex1.test(url)) {
-        const username = sessionStorage.getItem("useris");
-        const userinfo = JSON.parse(localStorage.getItem(username));
-        document.getElementById("hometitle").innerHTML = `${userinfo.churchname} Budget`;
+        var username = sessionStorage.getItem("useris");
+        var userinfo = JSON.parse(localStorage.getItem(username));
+        document.getElementById("hometitle").innerHTML = "".concat(userinfo.churchname, " Budget");
     }
     else {
         console.log('Something went wrong...');
     }
 }
 //displays and loads budgets for user
-const displaystuff = (name) => {
+var displaystuff = function (name) {
     var element = document.getElementById(name);
     if (element.style.display == "none") {
         element.style.display = "flex";
@@ -84,11 +86,11 @@ const displaystuff = (name) => {
 //create budget object in local storage
 function createbudget() {
     //assigns varibles to a budget object
-    const username = sessionStorage.getItem("useris");
-    const userinfo = JSON.parse(localStorage.getItem(username));
-    const bname = document.getElementById("create-bname").value;
-    const userbname = userinfo.username + bname;
-    const bamount = Number(document.getElementById("create-bamount").value);
+    var username = sessionStorage.getItem("useris");
+    var userinfo = JSON.parse(localStorage.getItem(username));
+    var bname = document.getElementById("create-bname").value;
+    var userbname = userinfo.username + bname;
+    var bamount = Number(document.getElementById("create-bamount").value);
     //makes sure that budget inputs are populated
     if (bname == "" || bamount == 0) {
         alert('Please fill out both budget feilds');
@@ -99,11 +101,11 @@ function createbudget() {
     }
     else {
         //Create and store new budget in local storage
-        const newbudget = new Budget(bname, bamount);
+        var newbudget = new classes_js_1.Budget(bname, bamount);
         localStorage.setItem(userbname, JSON.stringify(newbudget));
         //get user budgets array
-        const listname = userinfo.username + 'budgets';
-        const userbudgets = JSON.parse(localStorage.getItem(listname));
+        var listname = userinfo.username + 'budgets';
+        var userbudgets = JSON.parse(localStorage.getItem(listname));
         //store user budgets
         userbudgets.push(userbname);
         localStorage.setItem(listname, JSON.stringify(userbudgets));
